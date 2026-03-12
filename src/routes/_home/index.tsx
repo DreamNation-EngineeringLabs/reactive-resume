a728b2fcaac59a994ce03e0758429932d33e1d22
Venkatesh M <venkylm10@gmail.com>
2026-03-12 07:38:51 +0530

    feat: redirect home route to external URL and simplify component structure

--- src/routes/_home/index.tsx
+++ src/routes/_home/index.tsx
@@ -1,35 +1,18 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { DonationBanner } from "./-sections/donate";
-import { FAQ } from "./-sections/faq";
-import { Features } from "./-sections/features";
-import { Footer } from "./-sections/footer";
-import { Hero } from "./-sections/hero";
-import { Prefooter } from "./-sections/prefooter";
-import { Statistics } from "./-sections/statistics";
-import { Templates } from "./-sections/templates";
-import { Testimonials } from "./-sections/testimonials";
+import { useEffect } from "react";
 
 export const Route = createFileRoute("/_home/")({
 	component: RouteComponent,
 });
 
 function RouteComponent() {
-	return (
-		<main id="main-content" className="relative">
-			<Hero />
+	useEffect(() => {
+		window.location.href = "https://polymathai.co";
+	}, []);
 
-			<div className="container mx-auto px-4 sm:px-6 lg:px-12">
-				<div className="border-border border-x [&>section:first-child]:border-t-0 [&>section]:border-border [&>section]:border-t">
-					<Statistics />
-					<Features />
-					<Templates />
-					<Testimonials />
-					<DonationBanner />
-					<FAQ />
-					<Prefooter />
-					<Footer />
-				</div>
-			</div>
-		</main>
+	return (
+		<div className="flex h-screen items-center justify-center">
+			<div className="size-8 animate-spin rounded-full border-4 border-primary border-t-transparent" />
+		</div>
 	);
 }
